372c6db392e6a31c638cdd4f3469c79cb1a8be61
Lukawestfaldii <lakakawesi@gmail.com>
2024-11-24 11:43:15 +0400

    commit

---
+++ day 10/homework.py
@@ -0,0 +1,24 @@
+a = 3
+b = 7
+print(a < b)  
+ 
+a = 8
+b = 2
+print(a > b)
+
+a = 14
+b = 6
+print(a < b)
+
+a = 8
+b = 7
+print(a > b)
+
+a = 7
+b = 10
+print(a < b)
+
+age = 25
+has_id = True
+if age > 18: print("You are not allowed to enter.") 
+else:print("you are allowed to enter.")
