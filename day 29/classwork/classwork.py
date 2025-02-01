a5cfd8ede48686c1f6d67ab2c60fe46bac41af92
Lukawestfaldii <lakakawesi@gmail.com>
2025-02-01 19:15:47 +0400

    commit

---
+++ day 29/classwork/classwork.py
@@ -0,0 +1,6 @@
+# append  - აპენდი იმისთვის არის რომ ჩაამატოს რაიმე სიტყვა სიაში მაგალითად:
+fruits = ["apple", "banana", "cherry"]
+
+
+fruits.append("orange")
+print(fruits)
---
+++ day 31/classwork/class.py
@@ -0,0 +1,10 @@
+
+name = "გიორგი"
+surname = "თვალაძე"
+age = 25
+
+
+text = "ჩემი სახელია {}. ჩემი გვარია {} და ჩემი ასაკია {}.".format(name, surname, age)
+
+
+print(text)
